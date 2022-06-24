776f289f9e429a6febc66e2ea450a1fb60e7ab99
Shadowrunner11 <matematicadivina1@gmail.com>
2022-06-24 10:20:28 -0500

    [UPD] upd

--- js/video.js
+++ js/video.js
@@ -4,14 +4,33 @@ import { $ } from "./functions/selectors.js";
 const qrs = new URLSearchParams(document.location.search);
 
 qrs.append("key", KEY);
-
+const video =  $("#video-principal");
 fetch(`https://pixabay.com/api/videos/?${qrs}`)
     .then(response => response.json())
-    .then(({hits:[{videos:{large:{url}}}]}) =>{
-        $("#video-principal").setAttribute("src", url);
-        $("#video-principal").load();
-        $("#video-principal").addEventListener("loadeddata", (event)=>{
+    .then(({
+        hits:[{
+            videos:{
+                large:{
+                    url
+                }}
+        }]
+    }) =>{
+        video.setAttribute("src", url);
+        video.load();
+        video.addEventListener("loadeddata", (event)=>{
             event.preventDefault();
-            $("#video-principal").setAttribute("poster", null);
+            $(".lds-spinner").remove();
         });
     });
+
+
+$("#play").addEventListener("click",()=>{
+    if(video?.readyState){
+        if (video?.paused) {
+            video?.play();
+        } else {
+            video?.pause();
+        }
+
+    }
+});
